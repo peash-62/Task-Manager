8efbb94209c5aae70b398c8f19651485b6921987
peash-62 <sarkarpeash062@gmail.com>
2025-02-05 12:44:43 +0600

    in progress task screen ui added

--- lib/ui/screens/inprogress_task_screen.dart
+++ lib/ui/screens/inprogress_task_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:taskkmanager/ui/widgets/background_screen.dart';
+
+import '../widgets/task_list_item.dart';
 
 class InprogressTaskScreen extends StatefulWidget {
   const InprogressTaskScreen({super.key});
@@ -10,6 +13,18 @@ class InprogressTaskScreen extends StatefulWidget {
 class _InprogressTaskScreenState extends State<InprogressTaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenBackground(child: ListView.builder(
+        itemCount: 12,
+        itemBuilder: (context, index) {
+          return TaskListItem(
+            type: 'progress',
+            date: '24-02-2024',
+            subject: 'Title will be here',
+            description: 'description asedrfgj jkmj ghut nmk',
+            onDeletePress: () {},
+            onEditPress: () {},
+            Color: Colors.blueGrey,
+          );
+        }),);
   }
 }
