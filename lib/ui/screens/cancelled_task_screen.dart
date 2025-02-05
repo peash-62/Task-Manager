a02f77bf4a10d3db6da8f45e48a32cf05a9cbc4e
peash-62 <sarkarpeash062@gmail.com>
2025-02-05 12:43:58 +0600

    cancelled  task screen ui added

--- lib/ui/screens/cancelled_task_screen.dart
+++ lib/ui/screens/cancelled_task_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:taskkmanager/ui/widgets/background_screen.dart';
+
+import '../widgets/task_list_item.dart';
 
 class CancelledTaskScreen extends StatefulWidget {
   const CancelledTaskScreen({super.key});
@@ -10,6 +13,18 @@ class CancelledTaskScreen extends StatefulWidget {
 class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenBackground(child: ListView.builder(
+        itemCount: 12,
+        itemBuilder: (context, index) {
+          return TaskListItem(
+            type: 'cancelled',
+            date: '24-02-2024',
+            subject: 'Title will be here',
+            description: 'description asedrfgj jkmj ghut nmk',
+            onDeletePress: () {},
+            onEditPress: () {},
+            Color: Colors.redAccent,
+          );
+        }),);
   }
 }
