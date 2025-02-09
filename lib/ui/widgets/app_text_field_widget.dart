5142f3cca1a84b0ad8aa7fcf95c2d5d7ac03c140
peash-62 <sarkarpeash062@gmail.com>
2025-02-09 14:28:31 +0600

    form validation added

--- lib/ui/widgets/app_text_field_widget.dart
+++ lib/ui/widgets/app_text_field_widget.dart
@@ -6,7 +6,7 @@ class AppTextFieldWidget extends StatelessWidget {
     required this.hintText,
     required this.controller,
     this.obscuretext,
-    this.obscureText, this.maxLines,
+    this.obscureText, this.maxLines, this.validator,
   });
 
   final String hintText;
@@ -14,12 +14,19 @@ class AppTextFieldWidget extends StatelessWidget {
   final bool? obscureText;
   final dynamic obscuretext;
   final int? maxLines;
+  final Function(String?)? validator;
 
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       maxLines: maxLines ??1,
+      validator: (value){
+        if(validator != null){
+         return validator !(value);
+        }
+        return null;
+      },
       controller: controller,
       obscureText: obscureText ?? false,
       decoration: InputDecoration(
